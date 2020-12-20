168cb922b74b629e6f30a007877e4bea46682b04
Aditya Saxena <65659193+adityasaxena-crypto@users.noreply.github.com>
2020-12-21 00:33:01 +0530

    Update hbsscrapper.py

--- hbsscrapper.py
+++ hbsscrapper.py
@@ -1,19 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-
-source = requests.get('https://hbswk.hbs.edu/Pages/browse.aspx?HBSTopic=Finance').text
-soup = BeautifulSoup(source,'lxml')
-my_file_handle=open('maal.csv','w')
-csv_writer = csv.writer(my_file_handle)
-csv_writer.writerow(['headlines','authors','summary'])
-for article in soup.find_all('div',class_='highlight-container'):
-    authors = article.div.text
-    headline=article.h4.a.text
-    summary = article.p.text
-    print(summary)
-    print(authors)
-    print(headline)
-    csv_writer.writerow([headline,authors,summary])
+my_file_handle=open('hbs.csv','w')
+for i in range(27):
+  source = requests.get('https://hbswk.hbs.edu/Pages/browse.aspx?HBSTopic=Finance&page=i').text
+  soup = BeautifulSoup(source,'lxml')
+  
+  csv_writer = csv.writer(my_file_handle)
+  csv_writer.writerow(['headlines','authors','summary','link'])
+  for article in soup.find_all('div',class_='highlight-container'):
+      authors = article.div.text
+      headline=article.h4.a.text
+      summary = article.p.text
+      link=article.find('a',class_='hbsred')['href']
+      print(summary)
+      print(authors)
+      print(headline)
+      print(link)
+      csv_writer.writerow([headline,authors,summary,link])
     
 my_file_handle.close()
+
